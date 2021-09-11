4be0e48c04974ce16d764a7fe79deb3092aa2db4
Vlad Poberezhny <vlad.pbr@gmail.com>
2021-09-11 17:00:46 +0300

    fix(removed deprecated methods from utils)

--- pkg/codec/utils/utils.go
+++ pkg/codec/utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"encoding/gob"
 	"fmt"
 )
 
@@ -16,10 +15,6 @@ type Chunk struct {
 	ChunkID FourCC
 }
 
-func (c Chunk) GetBytesWithID(fields ...interface{}) []byte {
-	return append(GetBytes(false, c.ChunkID), GetBytes(false, fields...)...)
-}
-
 type ChunkInterface interface {
 	GetID() FourCC
 	Write(buffer *bytes.Buffer)
@@ -27,30 +22,6 @@ type ChunkInterface interface {
 
 func (c Chunk) GetID() FourCC {
 	return c.ChunkID
-}
-
-func GetBytes(zeroPad bool, fields ...interface{}) []byte {
-
-	var output []byte
-	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-
-	for _, field := range fields {
-
-		// convert interface to bytes
-		if err := encoder.Encode(field); err != nil {
-			panic(err)
-		}
-
-		output = append(output, buffer.Bytes()...)
-	}
-
-	// zero pad odd output if specified
-	if zeroPad && len(output)%2 != 0 {
-		output = append(output, byte(0))
-	}
-
-	return output
 }
 
 func ContainsFourCC(slice []FourCC, fourCC FourCC) bool {
@@ -61,11 +32,6 @@ func ContainsFourCC(slice []FourCC, fourCC FourCC) bool {
 	}
 
 	return false
-}
-
-// TODO implement
-func GetChunksBytes(chunks []ChunkInterface) []byte {
-	return []byte("")
 }
 
 // panicing version of buffer.Next
