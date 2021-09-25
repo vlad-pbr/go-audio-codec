504254ba7c43bab3f04bcb86112c7f06b023be93
Vlad Poberezhny <vlad.pbr@gmail.com>
2021-09-25 16:00:30 +0300

    feat(#3 implemented instrument chunk)

--- pkg/codec/format/aiff/instrument.go
+++ pkg/codec/format/aiff/instrument.go
@@ -3,6 +3,7 @@ package aiff
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 
 	"github.com/vlad-pbr/go-audio-codec/pkg/codec/utils"
 )
@@ -47,7 +48,37 @@ func (c InstrumentChunk) Write(buffer *bytes.Buffer) {
 	c.ReleaseLoop.Write(buffer)
 }
 
-// TODO implement
 func NewInstrumentChunk(buffer *bytes.Buffer) (utils.ChunkInterface, error) {
-	return InstrumentChunk{}, nil
+
+	// define chunk struct
+	var instChunk InstrumentChunk
+	instChunk.ChunkID = INSTRUMENTID
+	instChunk.ChunkSize = int32(binary.BigEndian.Uint32(utils.Next(buffer, 4)))
+
+	// make sure chunk size is 20
+	if instChunk.ChunkSize != 18 {
+		return instChunk, fmt.Errorf("%s chunk size is invalid: found %d, must be %d", string(INSTRUMENTID[:]), instChunk.ChunkSize, 20)
+	}
+
+	instChunk.BaseNote = int8(utils.Next(buffer, 1)[0])
+	instChunk.Detune = int8(utils.Next(buffer, 1)[0])
+	instChunk.LowNote = int8(utils.Next(buffer, 1)[0])
+	instChunk.HighNote = int8(utils.Next(buffer, 1)[0])
+	instChunk.LowVelocity = int8(utils.Next(buffer, 1)[0])
+	instChunk.HighVelocity = int8(utils.Next(buffer, 1)[0])
+	instChunk.Gain = int16(binary.BigEndian.Uint16(utils.Next(buffer, 2)))
+
+	instChunk.SustainLoop = Loop{
+		PlayMode:  int16(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+		BeginLoop: MarkerID(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+		EndLoop:   MarkerID(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+	}
+
+	instChunk.ReleaseLoop = Loop{
+		PlayMode:  int16(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+		BeginLoop: MarkerID(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+		EndLoop:   MarkerID(binary.BigEndian.Uint16(utils.Next(buffer, 2))),
+	}
+
+	return instChunk, nil
 }
